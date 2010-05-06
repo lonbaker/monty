cb54af579f9858977f46c443365a3691d0017e75
stonean <andy@stonean.com>
2010-05-05 20:27:06 -0400

    more tests on watch

--- test/monty/test_watch.rb
+++ test/monty/test_watch.rb
@@ -1,20 +1,105 @@
 require 'helper'
 
+class Authorization
+  extend Monty::Access
+end
+
 class TestMonty < Test::Unit::TestCase
+
+  def setup
+    Monty::Configuration.reset
+  end
 
   def app
     Monty::Watch.new(mock_framework)
   end
 
   def test_it_works
-    get '/', {}, {}
+    get '/'
     assert last_response.ok?
   end
 
   def test_it_redirects
-    get '/posts', {}, {'rack.session' => {:access_rights => "/blog/.*"}}
+    get '/posts'
     assert last_response.redirect?
   end
 
+  def test_it_allows_uri_without_ending_slash
+    Authorization.permission :posts
+    Authorization.public_access :posts
+
+    get '/posts', {}
+    assert last_response.ok?
+  end
+
+  def test_it_allows_uri_with_ending_slash
+    Authorization.permission :posts
+    Authorization.public_access :posts
+
+    get '/posts/', {}
+    assert last_response.ok?
+  end
+
+  def test_it_allows_uri_with_action
+    Authorization.permission :posts
+    Authorization.public_access :posts
+
+    get '/posts/new', {}
+    assert last_response.ok?
+
+    get '/posts', {}
+    assert last_response.ok?
+
+    get '/posts/', {}
+    assert last_response.ok?
+  end
+
+  def test_it_allows_uri_access_to_only_show
+    Authorization.permission :posts do
+      controller :posts do
+        only :show
+      end
+    end
+    Authorization.public_access :posts
+
+    get '/posts/show', {}
+    assert last_response.ok?
+
+    get '/postsshow', {}
+    assert last_response.redirect?
+
+    get '/posts', {}
+    assert last_response.redirect?
+
+    get '/posts/', {}
+    assert last_response.redirect?
+
+    get '/posts/edit', {}
+    assert last_response.redirect?
+  end
+
+  def test_it_allows_uri_access_to_all_except_show
+    Authorization.permission :posts do
+      controller :posts do
+        except :show
+      end
+    end
+    Authorization.public_access :posts
+
+    get '/posts/show', {}
+    assert last_response.redirect?
+
+    get '/postsshow', {}
+    assert last_response.redirect?
+
+    get '/posts', {}
+    assert last_response.ok?
+
+    get '/posts/', {}
+    assert last_response.ok?
+
+    get '/posts/edit', {}
+    assert last_response.ok?
+  end
 end
 
